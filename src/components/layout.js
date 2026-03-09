e36b3b1cfbf789a5bdf250dbb4b65b8e5a1ca73b
Yusufjon <yusupeu.dev@gmail.com>
2026-03-09 22:38:58 +0900

    feat: Add @vercel/analytics dependency and integrate analytics component in layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styled, { ThemeProvider } from 'styled-components';
 import { Head, Loader, Nav, Social, Email, Footer } from '@components';
 import { GlobalStyle, theme } from '@styles';
+import { Analytics } from '@vercel/analytics/next';
 
 const StyledContent = styled.div`
   display: flex;
@@ -56,6 +57,7 @@ const Layout = ({ children, location }) => {
       <div id="root">
         <ThemeProvider theme={theme}>
           <GlobalStyle />
+          <Analytics />
 
           <a className="skip-to-content" href="#content">
             Skip to Content
